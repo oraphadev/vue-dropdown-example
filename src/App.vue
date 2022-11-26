c9bdf0abee9733b5a9d96d8373b6813bab0b8023
Raphael Lima <oraphadev@gmail.com>
2022-11-26 11:18:32 -0300

    feat: add dropdown component

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <Dropdown />
+  </div>
+</template>
+
+<script>
+import Dropdown from '@/components/Dropdown'
+
+export default {
+  name: 'App',
+  components: {
+    Dropdown
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  font-family: 'Outfit';
+}
+
+body, html {
+  margin: 0;
+  padding: 0;
+  background: #121214;
+}
+
+span {
+  font-family: 'Material Symbols Outlined';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;
+  line-height: 1;
+  letter-spacing: normal;
+  text-transform: none;
+  display: inline-block;
+  white-space: nowrap;
+  word-wrap: normal;
+  direction: ltr;
+  font-feature-settings: 'liga';
+  -webkit-font-feature-settings: 'liga';
+  -webkit-font-smoothing: antialiased;
+}
+</style>
+
+<style scoped>
+#app {
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
---
+++ src/components/Dropdown.vue
@@ -0,0 +1,123 @@
+<template>
+  <div :class="['dropdown', { open }]">
+    <button
+      tabindex="0"
+      @focusout="close"
+      @click="toggle"
+    >
+      <span>tune</span>
+      Filtrar vagas
+      <span id="icon">{{ icon }}</span>
+    </button>
+    <ul>
+      <li>
+        <span>dns</span>
+        Backend
+      </li>
+      <li>
+        <span>palette</span>
+        Frontend
+      </li>
+      <li>
+        <span>bolt</span>
+        Fullstack
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheDropdown',
+  data: () => ({
+    open: false,
+  }),
+  computed: {
+    icon() {
+      return !this.open ? 'expand_more' : 'close'
+    }
+  },
+  methods: {
+    close() {
+      this.open = false
+    },
+    toggle() {
+      this.open = !this.open
+    },
+  },
+}
+</script>
+
+<style scoped>
+.dropdown,
+button {
+  position: relative;
+}
+
+.dropdown.open button {
+  background: #15a480;
+}
+
+.dropdown button {
+  z-index: 2;
+}
+
+.dropdown button #icon {
+  margin-left: auto;
+}
+
+button,
+li {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding: 0 16px;
+  width: 240px;
+  height: 64px;
+  border-radius: 6px;
+  color: #f9f9f9;
+  background: #252525;
+  border: 0;
+  cursor: pointer;
+  transition: 0.3s;
+  font-size: 16px;
+}
+
+button:hover{
+  background: #3a3a3a;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  position: absolute;
+  overflow: hidden;
+  z-index: 1;
+  top: 61px;
+  width: 100%;
+  opacity: 0;
+  visibility: hidden;
+  border-bottom-left-radius: 6px;
+  border-bottom-right-radius: 6px;
+  background: #3e3e3e;
+  translate: 0 -16px;
+  transition: 0.3s;
+}
+
+li {
+  width: 100%;
+  height: 56px;
+  border-radius: 0;
+}
+
+li:hover {
+  background: #383838;
+}
+
+.dropdown.open ul {
+  opacity: 1;
+  translate: 0;
+  visibility: visible;
+}
+</style>
